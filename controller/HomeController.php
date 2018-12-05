76d45fdc1bb7490172adfaac15e561ac4eb8772c
leon in 't Veld <leon3110l@gmail.com>
2018-12-05 13:56:59 +0100

    added read button

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -12,9 +12,19 @@ class HomeController {
     public function home($id = null) {
         $data = $this->employees->read($id);
 
+        $data = $this->addButtons($data);
+
         $table = HTMLElements::table($data, "table");
 
         include "view/table.php";
+    }
+
+    public function addButtons($array) {
+        foreach ($array as $key => $value) {
+            $array[$key]["actions"] = "<a href='".HTTP_DIR."/home/home/$value[employee_id]'>read</a>";
+        }
+
+        return $array;
     }
 
 }
